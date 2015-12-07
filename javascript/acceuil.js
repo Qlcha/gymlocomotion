102b47a6b7d4e2b47ff25952e7a6521429e6c191
fmeniass <massonflavy@yahoo.fr>
2015-12-07 09:49:36 -0500

    modification dimanche

--- javascript/acceuil.js
+++ javascript/acceuil.js
@@ -2,20 +2,11 @@
  * Created by fmeniass on 2015-11-30.
  */
 
-//code du slider
-
-// fin du code silder
-
 //image dynamique
 Premier = new Image(250,340);
 Premier.src = "images/gallerygym/transformation/femme-avant1.png";
 Second = new Image(250,340);
 Second.src = "images/gallerygym/transformation/femme-après1.png";
-
-premier = new Image(250,340);
-premier.src = "images/gallerygym/transformation/homme-avant1.png";
-second = new Image(250,340);
-second.src = "images/gallerygym/transformation/homme-après1.png";
 
 //fin image dynamique
 
@@ -41,6 +32,14 @@ function affiche(){
     document.getElementById("demo").innerHTML = "514-526-8941";
 }
 function affiche1(){
+
     document.getElementById("demo1").innerHTML = "gymlocomotion@gmail.com";
+}
+function premiere_lettre(){
+
+var mot = document.getElementById("majuscule");
+
+document.write(nom.charAt(0).toUpperCase());
+
 }
 
